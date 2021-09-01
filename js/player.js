879727958596f1f7e269faaa0c28285d024ba0f0
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-09-01 23:15:43 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,44 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+        
+        this.rank = null ;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    //Write a function for getPlayerAtEnd() - to read the data from the playerAtEnd node in the database.
+
+   //Write a function for updatePlayerAtEnd() - a static function that updates the playerAtEnd node in the database. 
+    
+}
